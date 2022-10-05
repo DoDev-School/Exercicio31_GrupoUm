3de96937c408ac53dc3c7f4df39300b0992de94d
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-10-04 21:11:00 -0300

    Passando a vez

--- script.ts
+++ script.ts
@@ -8,13 +8,13 @@ arrayPrincipal :Number
 arrayPar :Number
 arrayImpar :Number
 
-let arrayPrincipal = [2,3,1,5,7,4,8,9,0,10]
-let arrayPar = arrayPrincipal.filter(x => x%2 == 0)
-let arrayImpar = arrayPrincipal.filter(x => x%2 == 1)
+// let arrayPrincipal = [2,3,1,5,7,4,8,9,0,10]
+// let arrayPar = arrayPrincipal.filter(x => x%2 == 0)
+// let arrayImpar = arrayPrincipal.filter(x => x%2 == 1)
 
-console.log(arrayPrincipal)
-console.log(arrayPar)
-console.log(arrayImpar)
+// console.log(arrayPrincipal)
+// console.log(arrayPar)
+// console.log(arrayImpar)
 
 // 2. Faça um algoritmo com dois arrays A e B e preencha ambos com 10 elementos
 // cada. Depois salve em um terceiro array somente os números que estiverem nos
@@ -22,6 +22,23 @@ console.log(arrayImpar)
 // Ex.: arrayA[5,6,2,4,8,7,3,6,7,1]
 // arrayB[8,5,3,1,4,7,8,2,3,6]
 // Deve ser exibido no último array apenas os números arrayC[6,8,8,7,6,7]
+
+// arrayA : Number
+// arrayB : Number
+// arrayC : Number
+
+// let arrayA = [5,6,2,4,8,7,3,6,7,1]
+// let arrayB = [8,5,3,1,4,7,8,2,3,6]
+// let arrayC = []
+
+// arrayA.forEach(x => {
+//     arrayB.forEach(y => {
+//         if(x == y && x > 5){
+//             arrayC.push(x)
+//         }
+//     })    
+// })
+// console.log(arrayC)
 
 // 3. Crie uma classe Aluno com as propriedades: nome: string
 // idade: number
@@ -35,3 +52,24 @@ console.log(arrayImpar)
 // ● Que retorne o nome do aluno mais novo entre os homens;
 // ● Que retorne o primeiro Aluno que tiver a idade par ou tiver mais que 1,90 de
 // altura.
+
+class Aluno{
+    Nome : String
+    Idade : Number
+    Altura : Number
+    Sexo : String
+
+    constructor(nome, idade, altura, sexo){
+        this.Nome = nome
+        this.Idade = idade
+        this.Altura = altura
+        this.Sexo = sexo
+    }
+}
+
+arrayAluno : String
+let arrayAluno = []
+
+
+
+
